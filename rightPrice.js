6272125645e82579c5e540fa91f37b51f7ae4677
Thomas Nigon <[nigont@gmail.com](mailto:nigont@gmail.com)>
2023-05-21 15:45:54 +0200

    closing project as it is.

--- rightPrice.js
+++ rightPrice.js
@@ -3,40 +3,56 @@ function rightprice(){
     let player = prompt("what's your name?")
 
     // creation d'un nombre aléatoire
-    const rightPrice = Math.floor(Math.random() * 10) +1;
+    const rightPrice = Math.floor(Math.random() * 50) +1;
     console.log(rightPrice)
 
     // on demande au joueur de trouver le nombre aléatoire
-    let playerPrice = prompt(`please ${player} try to guess the price` )
+    let playerPrice = prompt(`please ${player} try to guess the price between 0 and 50` )
     /* let target = parseInt(playerPrice) */
     console.log(playerPrice )
 
     // creation d'une variable pour compter les tentatives
-    let attempt = 0;
+    let attempt = 1;
+    let maxAttempt = 4;
     console.log(attempt)
 
     // si le prix est bon  félicitation
-    if (rightPrice === playerPrice ){
+    if (rightPrice == playerPrice ){
         console.log("congratulation you made it first try!!!")
     }
     //sinon 
     else {  
         //tant que le prix est faux ou qu'on arrive a 15 essais on continue
-        while (playerPrice  != rightPrice || attempt===15){
-            
-            if (rightPrice>playerPrice ){
-                alert("c'est plus !")
-            }
-            else {alert("c'est moins !")
+        for ( attempt; attempt<=maxAttempt ; attempt++) /* || playerPrice != rightPrice) */{
+            console.log(attempt , maxAttempt)
+            console.log(playerPrice , rightPrice)
+            if (playerPrice == rightPrice){
+               
+                return  console.log("congratulation, you won !!")
+            }else if (rightPrice>playerPrice ){
+                alert(`attempt number ${attempt}: it's more !`)
+            }else {alert(`attempt number ${attempt}: it's less !`)
             }
             // on incrémente le nombre de tentatives
-            attempt++
+            //attempt++
             // on redemande un nombre a l'utilisateur
             playerPrice = prompt(`please ${player} try to guess the price` )
             
         }
-        console.log(`congratulation you won in ${attempt+1} attempts`)
-    }    
+        /* if (playerPrice ===rightPrice){
+            console.log("congratulation, you won !!")
+        } else { console.log("you lost")} */
+        
+        /* if (attempt===maxAttempt){
+            alert(`Oh no max attempts reached, you lost`)
+        console.log(`Oh no max attempts reached, you lost`)
+        }
+        else {
+            alert(`congratulation you won in ${attempt} attempts`)
+            console.log(`congratulation you won in ${attempt} attempts`)
+        }     */
+        }
+        
 }
 rightprice()
 
